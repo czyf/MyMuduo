9440557874c3052a0bfa3cee1eaaca1f0dcd6ed6
shiyue <shiyuepz@163.com>
2024-11-14 18:00:34 +0800

    add Timestamp class, and modifiy Logger.cpp

--- Logger.cpp
+++ Logger.cpp
@@ -1,4 +1,6 @@
 #include"Logger.h"
+#include"Timestamp.h"
+
 #include<string>
 #include<iostream>
 
@@ -17,19 +19,19 @@ void log(const std::string &msg){
     switch (loglevel_)
     {
     case INFO:
-        std::cout << "[INFO]";
+        std::cout << "[INFO]  ";
         break;
     case ERROR:
-        std::cout << "[ERROR]";
+        std::cout << "[ERROR]  ";
         break;
     case FATAL:
-        std::cout << "[FATAL]";
+        std::cout << "[FATAL]  ";
         break;
     case DEBUG:
-        std::cout << "[DEBUG]";
+        std::cout << "[DEBUG]  ";
         break;
     default:
         break;
     }
-    std::cout << "print time" << " : " << msg << std::endl;
+    std::cout << Timestamp::now().toString() << " : " << msg << std::endl;
 }
