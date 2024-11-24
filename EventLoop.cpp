9454a25db444a41dd109371eb6d7b10d1062d306
shiyue <shiyuepz@163.com>
2024-11-24 14:06:01 +0800

    add EventLoop

--- EventLoop.cpp
+++ EventLoop.cpp
@@ -1,1 +1,9 @@
 #include"EventLoop.h"
+
+// 防止一个线程创建多个EventLoop  thread_local
+__thread EventLoop *t_loopInThisThread = nullptr;
+
+// 定义默认的Poller IO复用接口的超时时间
+const int kPollTimeMs  = 10000;
+
+int create
